387e69b1a48aa0b442ab6e54b2f13554500c5d69
Klaus <85108209+Chernoburkv@users.noreply.github.com>
2022-09-07 16:27:49 +0300

    Create TDCX.ino

---
+++ TDCX.ino
@@ -0,0 +1,334 @@
+Oscilloscope Clock 
+  using internal DACs, with WiFi and ntp sync.
+  
+  Mauro Pintus , Milano 2018/05/25
+  How to use it:
+  Load this sketch on a ESP32 board using the Arduino IDE 1.8.7
+  See Andreas Spiess video linked below if you dont know how to...
+  Connect your oscilloscope channels to GPIO25 and GPIO26 of the ESP32
+  Connect the ground of the oscilloscope to the GND of the ESP32 board
+  Put your Oscilloscope in XY mode
+  Adjust the vertical scale of the used channels to fit the clock
+  Enjoy Your new Oscilloscope Clock!!! :)
+  Additional notes:
+  By default this sketch will start from a fix time 10:08:37 everityme 
+  you reset the board.
+  To change it, modify the variables h,m,s below.
+  To synchronize the clock with an NTP server, you have to install 
+  the library NTPtimeESP from Andreas Spiess.
+  Then ncomment the line //#define NTP, removing the //.
+  Edit the WiFi credential in place of Your SSID and Your PASS.
+  Check in the serial monitor if it can reach the NTP server.
+  You mignt need to chouse a different pool server for your country.
+  If you want there is also a special mode that can be enabled uncommenting 
+  the line //#define EXCEL, removing the //. In this mode, the sketch
+  will run once and will output on the serial monitor all the coordinates
+  it has generated. You can use this coordinates to draw the clock 
+  using the graph function in Excel or LibreOffice
+  This is useful to test anything you want to display on the oscilloscope
+  to verify the actual points that will be generated.
+  GitHub Repository
+  https://github.com/maurohh/ESP32_OscilloscopeClock
+  Twitter Page
+  https://twitter.com/PintusMauro
+  Youtube Channel
+  www.youtube.com/channel/UCZ93JYpVb9rEbg5cbcVG_WA/
+  Old Web Site
+  www.mauroh.com
+  Credits:
+  Andreas Spiess
+  https://www.youtube.com/watch?v=DgaKlh081tU
+  Andreas Spiess NTP Library
+  https://github.com/SensorsIot/NTPtimeESP
+  
+  My project is based on this one:
+  http://www.dutchtronix.com/ScopeClock.htm
+  
+  Thank you!!
+******************************************************************************/
+
+#include "DataTable.h"
+
+#define BlankPin   34  // high blanks the display
+#define RelPin  4  // Relay HV out
+#define LedPin 2 // ledd
+
+
+
+// Change this to set the initial Time
+// Now is 10:08:37 (12h)
+int h=10;   //Start Hour 
+int m=8;    //Start Minutes
+int s=37;   //Start Seconds
+
+//Variables
+int           lastx,lasty;
+unsigned long currentMillis  = 0;
+unsigned long previousMillis = 0;    
+int           Timeout        = 20;
+const    long interval       = 990; //milliseconds, you should twick this
+                                    //to get a better accuracy
+
+
+//*****************************************************************************
+// PlotTable 
+//*****************************************************************************
+
+void PlotTable(byte *SubTable, int SubTableSize, int skip, int opt, int offset)
+{
+  int i=offset;
+  while (i<SubTableSize){
+    if (SubTable[i+2]==skip){
+      i=i+3;
+      if (opt==1) if (SubTable[i]==skip) i++;
+    }
+    Line(SubTable[i],SubTable[i+1],SubTable[i+2],SubTable[i+3]);  
+    if (opt==2){
+      Line(SubTable[i+2],SubTable[i+3],SubTable[i],SubTable[i+1]); 
+    }
+    i=i+2;
+    if (SubTable[i+2]==0xFF) break;
+  }
+}
+
+// End PlotTable 
+//*****************************************************************************
+
+
+
+//*****************************************************************************
+// Dot 
+//*****************************************************************************
+
+inline void Dot(int x, int y)
+{
+    if (lastx!=x){
+      lastx=x;
+      dac_output_voltage(DAC_CHANNEL_1, x);
+    }
+    
+    if (lasty!=y){
+      lasty=y;
+      dac_output_voltage(DAC_CHANNEL_2, y);
+    }
+}
+
+// End Dot 
+//*****************************************************************************
+
+
+
+//*****************************************************************************
+// Line 
+//*****************************************************************************
+// Bresenham's Algorithm implementation optimized
+// also known as a DDA - digital differential analyzer
+
+void Line(byte x1, byte y1, byte x2, byte y2)
+{
+    int acc;
+    // for speed, there are 8 DDA's, one for each octant
+    if (y1 < y2) { // quadrant 1 or 2
+        byte dy = y2 - y1;
+        if (x1 < x2) { // quadrant 1
+            byte dx = x2 - x1;
+            if (dx > dy) { // < 45
+                acc = (dx >> 1);
+                for (; x1 <= x2; x1++) {
+                    Dot(x1, y1);
+                    acc -= dy;
+                    if (acc < 0) {
+                        y1++;
+                        acc += dx;
+                    }
+                }
+            }
+            else {   // > 45
+                acc = dy >> 1;
+                for (; y1 <= y2; y1++) {
+                    Dot(x1, y1);
+                    acc -= dx;
+                    if (acc < 0) {
+                        x1++;
+                        acc += dy;
+                    }
+                }
+            }
+        }
+        else {  // quadrant 2
+            byte dx = x1 - x2;
+            if (dx > dy) { // < 45
+                acc = dx >> 1;
+                for (; x1 >= x2; x1--) {
+                    Dot(x1, y1);
+                    acc -= dy;
+                    if (acc < 0) {
+                        y1++;
+                        acc += dx;
+                    }
+                }
+            }
+            else {  // > 45
+                acc = dy >> 1;
+                for (; y1 <= y2; y1++) {
+                    Dot(x1, y1);
+                    acc -= dx;
+                    if (acc < 0) {
+                        x1--;
+                        acc += dy;
+                    }
+                }
+            }        
+        }
+    }
+    else { // quadrant 3 or 4
+        byte dy = y1 - y2;
+        if (x1 < x2) { // quadrant 4
+            byte dx = x2 - x1;
+            if (dx > dy) {  // < 45
+                acc = dx >> 1;
+                for (; x1 <= x2; x1++) {
+                    Dot(x1, y1);
+                    acc -= dy;
+                    if (acc < 0) {
+                        y1--;
+                        acc += dx;
+                    }
+                }
+            
+            }
+            else {  // > 45
+                acc = dy >> 1;
+                for (; y1 >= y2; y1--) { 
+                    Dot(x1, y1);
+                    acc -= dx;
+                    if (acc < 0) {
+                        x1++;
+                        acc += dy;
+                    }
+                }
+
+            }
+        }
+        else {  // quadrant 3
+            byte dx = x1 - x2;
+            if (dx > dy) { // < 45
+                acc = dx >> 1;
+                for (; x1 >= x2; x1--) {
+                    Dot(x1, y1);
+                    acc -= dy;
+                    if (acc < 0) {
+                        y1--;
+                        acc += dx;
+                    }
+                }
+
+            }
+            else {  // > 45
+                acc = dy >> 1;
+                for (; y1 >= y2; y1--) {
+                    Dot(x1, y1);
+                    acc -= dx;
+                    if (acc < 0) {
+                        x1--;
+                        acc += dy;
+                    }
+                }
+            }
+        }
+    
+    }
+}
+
+// End Line 
+//*****************************************************************************
+
+
+
+//*****************************************************************************
+// setup 
+//*****************************************************************************
+
+void setup() 
+{
+  pinMode(BlankPin, OUTPUT);   
+  pinMode(LedPin, OUTPUT);
+  pinMode(RelPin, OUTPUT);
+  
+  
+  
+  dac_output_enable(DAC_CHANNEL_1);
+  dac_output_enable(DAC_CHANNEL_2);
+
+  if (h > 12) h=h-12;
+  h=(h*5)+m/12;
+  
+  delay(250);
+  digitalWriteFast(LedPin, HIGH);
+  delay(7000);
+  digitalWriteFast(RelPin, HIGH);
+  delay(250);
+  digitalWriteFast(BlankPin, HIGH);
+
+
+}
+
+// End setup 
+//*****************************************************************************
+
+
+
+//*****************************************************************************
+// loop 
+//*****************************************************************************
+
+void loop() {
+
+  currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    s++;
+  }
+  if (s==60) {
+    s=0;
+    m++;
+    if ((m==12)||(m==24)||(m==36)||(m==48)) {
+      h++;
+    }
+  }
+  if (m==60) {
+    m=0;
+    h++;
+  }
+  if (h==60) {
+    h=0;
+  }
+
+  //Optionals
+  //PlotTable(DialDots,sizeof(DialDots),0x00,1,0);
+  //PlotTable(TestData,sizeof(TestData),0x00,0,00); //Full
+  //PlotTable(TestData,sizeof(TestData),0x00,0,11); //Without square
+
+  int i;
+  PlotTable(DialData,sizeof(DialData),0x00,1,0);      //2 to back trace
+  PlotTable(DialDigits12,sizeof(DialDigits12),0x00,1,0);//2 to back trace 
+  PlotTable(HrPtrData, sizeof(HrPtrData), 0xFF,0,9*h);  // 9*h
+  PlotTable(MinPtrData,sizeof(MinPtrData),0xFF,0,9*m);  // 9*m
+  PlotTable(SecPtrData,sizeof(SecPtrData),0xFF,0,5*s);  // 5*s
+}
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
